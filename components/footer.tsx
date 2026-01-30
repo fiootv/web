c465c8b4ea072892149726d4160e444dbba22c3d
NEER4J <ittsneeraj@gmail.com>
2026-01-31 02:24:07 +0530

    d

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,7 +6,7 @@ import { Button } from "@/components/ui/button";
 import { Facebook, Mail, Phone, ArrowRight } from "lucide-react";
 
 export function Footer() {
-  const currentYear = 2025;
+  const currentYear = new Date().getFullYear();
 
   const footerLinks = {
     company: [
